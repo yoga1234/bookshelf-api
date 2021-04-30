d150424d9f1902e423f7816ad6d30f81f90edddb
Septiana Yoga <septianayoga30@gmail.com>
2021-04-30 09:20:52 +0700

    adding edit data function

--- handler.js
+++ handler.js
@@ -94,4 +94,51 @@ const getBookByIdHandler = (request, h) => {
   return getBookWithIdCheck(h)
 }
 
-module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler }
+const editBookByIdHandler = (request, h) => {
+  const { id } = request.params
+
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading
+  } = request.payload
+  const updatedAt = new Date().toISOString()
+
+  const index = books.findIndex((book) => book.id === id)
+
+  if(index !== -1) {
+    books[index] = {
+      ...books[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
+      updatedAt
+    }
+
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbaharui'
+    })
+    response.code(200)
+    return response
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbaharui Buku. Id tidak ditemukan'
+  })
+  response.code(404)
+  return response
+}
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler, editBookByIdHandler }
